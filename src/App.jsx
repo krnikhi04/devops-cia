0dfa03691546909adeba3392d5c889ee6fd0cdd8
krnikhi04 <nikhitha22110243@snuchennai.edu.in>
2025-11-12 08:48:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Hi, CI/CD!</h1>
+      <h1>Hey, CI/CD!</h1>
       <p>My React app is running from Jenkins!</p>
       <h1>hello</h1>
     </div>
